9cf907d12d7832e8ec7c19e26bee971440009863
Pedro Caixeta <pedro.mg.caixeta@gmail.com>
2023-06-10 16:46:45 -0300

    adding content.js with initial logic

---
+++ content.js
@@ -0,0 +1,9 @@
+switch (window.location.hostname) {
+    case "www.youtube.com":
+        alert("we are in youtube");
+        break;
+
+    case "www.netflix.com":
+        alert("we are in netflix");
+        break;
+}
